451267b6de9c94845b54d65221f1c47d5f499fc5
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-16 12:32:02 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,38 +1,28 @@
 {
   description = "Tools for HPP";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    systems.follows = "gepetto/systems";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros.overrideAttrs.hpp-tools = _: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./bin
-                  ./blender
-                  ./CMakeLists.txt
-                  ./etc
-                  ./gdb
-                  ./hpp
-                  ./install
-                  ./package.xml
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-tools = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./bin
+              ./blender
+              ./CMakeLists.txt
+              ./etc
+              ./gdb
+              ./hpp
+              ./install
+              ./package.xml
+            ];
+          };
+        };
       }
     );
 }
