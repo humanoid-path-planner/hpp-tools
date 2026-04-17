bec23531fec2772489b3b06e196c26b76e099205
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-17 11:25:01 +0200

    fix v8.0.0 merge

--- flake.nix
+++ flake.nix
@@ -1,42 +1,28 @@
 {
   description = "Tools for HPP";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    gazebros2nix.follows = "gepetto/gazebros2nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            gazebros2nix.overrides.hpp-tools = _final: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./bin
-                  ./blender
-                  ./CMakeLists.txt
-                  ./etc
-                  ./gdb
-                  ./hpp
-                  ./install
-                  ./package.xml
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-tools = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./bin
+              ./blender
+              ./CMakeLists.txt
+              ./etc
+              ./gdb
+              ./hpp
+              ./install
+              ./package.xml
+            ];
+          };
+        };
       }
     );
 }
